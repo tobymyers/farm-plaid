fc34513e7cfc8e84ab4d3ad398cac94332822708
Toby Myers <toby.isaac.myers@gmail.com>
2023-12-26 11:11:32 -0600

    updated env variables to allow for heroku changing database url

--- config_prod.py
+++ config_prod.py
@@ -1,12 +1,19 @@
 
 
-
-
-
-# config_prod.py
+import os
 
 class Config:
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://qwwyjzvsvlyzqj:d6edefaa80e6c75e2782acd4f8327ac5f7f2d714c6130b6ce6c4a19ed7e1c365@ec2-52-4-153-146.compute-1.amazonaws.com:5432/delcr43okma9ir'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # Other production configurations
-    CLOUD_DATABASE_URL = SQLALCHEMY_DATABASE_URI
+    CLOUD_DATABASE_URL = os.environ.get('DATABASE_URL')
+
+
+
+# # config_prod.py
+
+# class Config:
+#     DEBUG = False
+#     SQLALCHEMY_DATABASE_URI = 'postgresql://qwwyjzvsvlyzqj:d6edefaa80e6c75e2782acd4f8327ac5f7f2d714c6130b6ce6c4a19ed7e1c365@ec2-52-4-153-146.compute-1.amazonaws.com:5432/delcr43okma9ir'
+#     # Other production configurations
+#     CLOUD_DATABASE_URL = SQLALCHEMY_DATABASE_URI
